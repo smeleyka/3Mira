1df6246aaf539dc2b035c504985ad6dec0c6a847
smeleyka <smeleyka@gmail.com>
2018-03-19 18:25:26 +0300

    Initial commit

---
+++ app/src/main/java/ru/smeleyka/a3mira/MainActivity.java
@@ -0,0 +1,35 @@
+package ru.smeleyka.a3mira;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.web_view)
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        getSupportActionBar().hide();
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://tri-mira.ru");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
---
+++ app/src/main/java/ru/smeleyka/a3mira/MyWebViewClient.java
@@ -0,0 +1,18 @@
+package ru.smeleyka.a3mira;
+
+import android.annotation.TargetApi;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by smeleyka on 19.03.18.
+ */
+
+public class MyWebViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+}
