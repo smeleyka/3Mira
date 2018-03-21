7ed343d70d947710cdaa2eeecb29b1a0812b1ba7
smeleyka <smeleyka@gmail.com>
2018-03-21 18:07:31 +0300

    init

--- app/src/main/java/ru/smeleyka/a3mira/MainActivity.java
+++ app/src/main/java/ru/smeleyka/a3mira/MainActivity.java
@@ -2,22 +2,33 @@ package ru.smeleyka.a3mira;
 
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.MenuCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+
+
+import com.jakewharton.rxbinding2.support.design.widget.RxBottomNavigationView;
+
+import org.reactivestreams.Subscription;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import butterknife.OnTouch;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity.class";
 
-    @BindView(R.id.bottom_nav) BottomNavigationView mBottomNavigationView;
-    @BindView(R.id.web_view) WebView webView;
+    @BindView(R.id.bottom_nav)   BottomNavigationView mBottomNavigationView;
+    @BindView(R.id.web_view)    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +38,40 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         webView.setWebViewClient(new MyWebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl(getString(R.string.url));
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setAppCachePath(getCacheDir().getAbsolutePath());
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        //webView.loadUrl(getString(R.string.url));
 
-        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) mBottomNavigationView.getLayoutParams();
-        layoutParams.setBehavior(new BottomNavigationViewBehavior());
+        Disposable buttomNavigationViewSub = RxBottomNavigationView.itemSelections(mBottomNavigationView)
+                .subscribe(menuItem -> {
+                    switch (menuItem.getItemId()) {
+                        case R.id.action_home:
+                            webView.loadUrl(getString(R.string.url_home));
+                            Log.d(TAG,"url_home");
+                            break;
+                        case R.id.action_route:
+                            webView.loadUrl(getString(R.string.url_route));
+                            Log.d(TAG,"url_route");
+                            break;
+                        case R.id.action_faq:
+                            webView.loadUrl(getString(R.string.url_faq));
+                            Log.d(TAG,"url_faq");
+                            break;
+                        default:
+                            break;
+                    }
+                });
     }
 
     @OnClick(R.id.web_view)
-    public void onWebViewClick(){
-       Log.d(TAG,webView.getScrollX()+" "+webView.getScrollY());
+    public void onWebViewClick() {
+        Log.d(TAG, webView.getScrollX() + " " + webView.getScrollY());
     }
 
     @Override
     public void onBackPressed() {
-        if(webView.canGoBack()) {
+        if (webView.canGoBack()) {
             webView.goBack();
         } else {
             super.onBackPressed();
