7f8e0912d7b25fc55002eee93be10c9e886a7f45
smeleyka <smeleyka@gmail.com>
2018-03-30 17:54:17 +0300

    1.04

--- app/src/main/java/ru/smeleyka/a3mira/MainActivity.java
+++ app/src/main/java/ru/smeleyka/a3mira/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.smeleyka.a3mira;
 
 import android.annotation.TargetApi;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -26,11 +28,18 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity.class";
 
-    @BindView(R.id.bottom_navigation_view)   BottomNavigationView mBottomNavigationView;
-    @BindView(R.id.web_view)                 WebView webView;
-    @BindView(R.id.toolbar)                  Toolbar toolbar;
-    @BindView(R.id.drawer_layout)            DrawerLayout drawer;
-    @BindView(R.id.drawer_navigation_view)   NavigationView drawerNavigationView;
+    @BindView(R.id.bottom_navigation_view)
+    BottomNavigationView mBottomNavigationView;
+    @BindView(R.id.web_view)
+    WebView webView;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+    @BindView(R.id.drawer_navigation_view)
+    NavigationView drawerNavigationView;
+    @BindView(R.id.appbar)
+    AppBarLayout appbar;
 
 
     @Override
@@ -50,33 +59,32 @@ public class MainActivity extends AppCompatActivity {
         initDrawerNavigationController();
 
 
-
     }
 
     private void initDrawerNavigationController() {
         RxNavigationView.itemSelections(drawerNavigationView).subscribe(menuItem -> {
-        switch (menuItem.getItemId()) {
-            case R.id.drawer_action_home:
-                webView.loadUrl(getString(R.string.url_home));
-                closeDrawer();
-                Log.d(TAG,"url_home");
-                break;
-            case R.id.drawer_action_route:
-                webView.loadUrl(getString(R.string.url_route));
-                closeDrawer();
+            switch (menuItem.getItemId()) {
+                case R.id.drawer_action_home:
+                    webView.loadUrl(getString(R.string.url_home));
+                    closeDrawer();
+                    Log.d(TAG, "url_home");
+                    break;
+                case R.id.drawer_action_route:
+                    webView.loadUrl(getString(R.string.url_route));
+                    closeDrawer();
 
-                Log.d(TAG,"url_route");
-                break;
-            case R.id.drawer_action_faq:
-                webView.loadUrl(getString(R.string.url_faq));
-                closeDrawer();
+                    Log.d(TAG, "url_route");
+                    break;
+                case R.id.drawer_action_faq:
+                    webView.loadUrl(getString(R.string.url_faq));
+                    closeDrawer();
 
-                Log.d(TAG,"url_faq");
-                break;
-            default:
-                break;
-        }
-    });
+                    Log.d(TAG, "url_faq");
+                    break;
+                default:
+                    break;
+            }
+        });
     }
 
     private void closeDrawer() {
@@ -85,24 +93,24 @@ public class MainActivity extends AppCompatActivity {
 
     private void initBottomNavigationController() {
         Disposable buttomNavigationViewSub = RxBottomNavigationView.itemSelections(mBottomNavigationView)
-            .subscribe(menuItem -> {
-                switch (menuItem.getItemId()) {
-                    case R.id.action_home:
-                        webView.loadUrl(getString(R.string.url_home));
-                        Log.d(TAG,"url_home");
-                        break;
-                    case R.id.action_route:
-                        webView.loadUrl(getString(R.string.url_route));
-                        Log.d(TAG,"url_route");
-                        break;
-                    case R.id.action_faq:
-                        webView.loadUrl(getString(R.string.url_faq));
-                        Log.d(TAG,"url_faq");
-                        break;
-                    default:
-                        break;
-                }
-            });
+                .subscribe(menuItem -> {
+                    switch (menuItem.getItemId()) {
+                        case R.id.action_home:
+                            webView.loadUrl(getString(R.string.url_home));
+                            Log.d(TAG, "url_home");
+                            break;
+                        case R.id.action_route:
+                            webView.loadUrl(getString(R.string.url_route));
+                            Log.d(TAG, "url_route");
+                            break;
+                        case R.id.action_faq:
+                            webView.loadUrl(getString(R.string.url_faq));
+                            Log.d(TAG, "url_faq");
+                            break;
+                        default:
+                            break;
+                    }
+                });
     }
 
     private void initWebView() {
@@ -114,17 +122,17 @@ public class MainActivity extends AppCompatActivity {
         webView.setLayoutAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-                Log.d(TAG,"onAnimationStart");
+                Log.d(TAG, "onAnimationStart");
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                Log.d(TAG,"onAnimationEnd");
+                Log.d(TAG, "onAnimationEnd");
             }
 
             @Override
             public void onAnimationRepeat(Animation animation) {
-                Log.d(TAG,"onAnimationRepeat");
+                Log.d(TAG, "onAnimationRepeat");
             }
         });
 
@@ -147,31 +155,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @TargetApi(23)
-    public void onWebViewScroll(){
+    public void onWebViewScroll() {
         webView.setOnScrollChangeListener((v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
-            Log.d(TAG,"scrollX"+String.valueOf(scrollX));
-            Log.d(TAG,"scrollY"+String.valueOf(scrollY));
-            Log.d(TAG,"oldScrollX"+String.valueOf(oldScrollX));
-            Log.d(TAG,"oldScrollY"+String.valueOf(oldScrollY));
-            showBottomNavigation(oldScrollY<scrollY);
+            Log.d(TAG, scrollY + " - " + oldScrollY);
+            showBottomNavigation(oldScrollY < scrollY);
         });
     }
 
-    public void showBottomNavigation(boolean show){
-       if(show) {
-           mBottomNavigationView.animate().translationY(mBottomNavigationView.getHeight());
+    @TargetApi(23)
+    public void showBottomNavigation(boolean show) {
+        if (show) {
+            if (mBottomNavigationView.getTranslationY() == 0) {
+                mBottomNavigationView.animate().translationY(mBottomNavigationView.getHeight());
+                appbar.animate().translationY(appbar.getHeight()*-1);
+                Log.d(TAG, "Show BAR  " + mBottomNavigationView.getTranslationY());
+            }
+            //mBottomNavigationView.setVisibility(View.INVISIBLE);
+        }
+        if (!show) {
+            if (mBottomNavigationView.getTranslationY() == mBottomNavigationView.getHeight()) {
+                mBottomNavigationView.animate().translationY(0);
+                appbar.animate().translationY(0);
 
-           //mBottomNavigationView.setVisibility(View.INVISIBLE);
-       }
+                Log.d(TAG, "Show BAR  " + mBottomNavigationView.getTranslationY());
+            }
 
-       else if (!show){
-           mBottomNavigationView.animate().translationY(0);
 
-          //mBottomNavigationView.setVisibility(View.VISIBLE);
-       }
+            //mBottomNavigationView.setVisibility(View.VISIBLE);
+        }
     }
-
-
 
 
 }
